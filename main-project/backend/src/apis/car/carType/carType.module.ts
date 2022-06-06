78735a3122054981c0c8f5308651b3d7319c379e
LeoKim <leo3179@naver.com>
2022-06-06 19:16:58 +0900

    💚 fix build

--- main-project/backend/src/apis/car/carType/carType.module.ts
+++ main-project/backend/src/apis/car/carType/carType.module.ts
@@ -6,23 +6,23 @@ import { CarTypeService } from './carType.service';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([CarType]),
-        ElasticsearchModule.register({
-            node: 'https://search-my-elasticsearch-ce5vfgm4ifqvzhhxsrqjnveasm.us-west-2.es.amazonaws.com:443', //node는 컴퓨터라는 뜻
-            auth:{
-                username: "leoelasticsearch",
-                password: "Leo1178!"
-            },
-            headers:{
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }),
-    ],
-    providers: [
-        CarTypeService, //여기에 의존성 주입
-        CarTypeResolver,
-    ],
+  imports: [
+    TypeOrmModule.forFeature([CarType]),
+    ElasticsearchModule.register({
+      node: 'https://search-my-elasticsearch-ce5vfgm4ifqvzhhxsrqjnveasm.us-west-2.es.amazonaws.com:443', //node는 컴퓨터라는 뜻
+      auth: {
+        username: 'leoelasticsearch',
+        password: 'Leo1178!',
+      },
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }),
+  ],
+  providers: [
+    CarTypeService, //여기에 의존성 주입
+    CarTypeResolver,
+  ],
 })
 export class CarTypeModule {}
